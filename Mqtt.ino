b74aeccc67ff823b13ffa5a5db5aab1da5254aff
Richard <rnolec@gmail.com>
2017-04-12 11:53:16 -0500

    Added mew foñe Mqtt.ino

---
+++ Mqtt.ino
@@ -0,0 +1,43 @@
+
+/*  Mqtt_reconnect
+ *  Espera hasta que el cliente mqtt se haya
+ *  conectado; una vez hecho eso, se subscribe
+ *  al topic "topic/test"
+*/
+void Mqtt_reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("topic/test");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+/*  Mqtt_callback
+ *  Esta funcion es llamada cada vez que se recibe un topic
+ *  con un payload
+*/
+void Mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+}
+
+
